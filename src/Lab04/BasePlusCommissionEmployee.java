b488418d1d968fedd39d1e089726a63830387549
Bo Qu <qu000030@algonquinlive.com>
2024-05-27 00:14:48 -0400

    finish lab 4

--- src/Lab04/BasePlusCommissionEmployee.java
+++ src/Lab04/BasePlusCommissionEmployee.java
@@ -1,8 +1,8 @@
 /**
  * Student Name: Bo Qu
  * Lab Professor: Travis Lothar Czech
- * Due Date: 2024-05-24
- * Modified: 2024-05-24
+ * Due Date: 2024-06-02
+ * Modified: 2024-05-26
  * Description: Lab assignment 04
  */
 
@@ -10,63 +10,56 @@ package Lab04;
 
 public class BasePlusCommissionEmployee {
     private double baseSalary; // base salary per week
+    private final CommissionEmployee commissionEmployee;
 
     // six-argument constructor
     public BasePlusCommissionEmployee(String first, String last,
                                       String ssn, double sales, double rate, double salary) {
-        /* construct the CommissionEmployee portion of this object */
+        // construct the CommissionEmployee object
+        this.commissionEmployee = new CommissionEmployee(first, last, ssn, sales, rate);
         setBaseSalary(salary); // validate and store base salary
     }
 
     // get first name
     public String getFirstName() {
         /* get the first name of the composed CommissionEmployee object */
-
-    }
-
-    // set first name
-    public void setFirstName(String first) {
-        /* set the first name of the composed CommissionEmployee object */
+        return commissionEmployee.getFirstName();
     }
 
     // get last name
     public String getLastName() {
         /* get the last name of the composed CommissionEmployee object */
-    }
-
-    // set last name
-    public void setLastName(String last) {
-        /* set the last name of the composed CommissionEmployee object */
+        return commissionEmployee.getLastName();
     }
 
     // get social security number
     public String getSocialSecurityNumber() {
         /* get the social security number of the composed CommissionEmployee object */
-    }
-
-    // set social security number
-    public void setSocialSecurityNumber(String ssn) {
-        /* set the social security number of the composed CommissionEmployee object */
+        return commissionEmployee.getSocialSecurityNumber();
     }
 
     // get gross sales amount
     public double getGrossSales() {
         /* get the gross sales amount of the composed CommissionEmployee object */
+        return commissionEmployee.getGrossSales();
     }
 
     // set gross sales amount
     public void setGrossSales(double sales) {
         /* set the gross sales amount of the composed CommissionEmployee object */
+        commissionEmployee.setGrossSales(sales);
     }
 
     // get commission rate
     public double getCommissionRate() {
         /* get the commission rate of the composed CommissionEmployee object */
+        return commissionEmployee.getCommissionRate();
     }
 
     // set commission rate
     public void setCommissionRate(double rate) {
         /* set the commission rate of the composed CommissionEmployee object */
+        commissionEmployee.setCommissionRate(rate);
     }
 
     // get base salary
@@ -84,11 +77,19 @@ public class BasePlusCommissionEmployee {
     // calculate earnings
     public double earnings() {
         /* calculate the earnings of the composed CommissionEmployee object */
+        return baseSalary + commissionEmployee.earnings();
     }
 
     // return String representation of BasePlusCommissionEmployee object
     public String toString() {
-        /* Return a string consisting of the string representation of the composed
+          /* Return a string consisting of the string representation of the composed
         CommissionEmployee object along with the base salary */
+        return String.format("%s: %s %s%n%s: %s%n%s: %.2f%n%s: %.2f%n%s: %.2f%n%s: %.2f",
+                "commission employee", getFirstName(), getLastName(),
+                "social security number", getSocialSecurityNumber(),
+                "gross sales", commissionEmployee.getGrossSales(),
+                "commission rate", commissionEmployee.getCommissionRate(),
+                "base salary", baseSalary,
+                "earnings", earnings());
     }
 }
