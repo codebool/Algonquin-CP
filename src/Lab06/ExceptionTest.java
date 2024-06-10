b74898a398421fff2030c000a27fbc9beb3ea75f
Bo Qu <qu000030@algonquinlive.com>
2024-06-10 11:21:25 -0400

    finish lab 6

--- src/Lab06/ExceptionTest.java
+++ src/Lab06/ExceptionTest.java
@@ -8,12 +8,39 @@
 
 package Lab06;
 
+import java.util.Scanner;
+
+// Custom exception class
 public class ExceptionTest {
+    // Quotient method that throws an ArithmeticException, which is a built-in exception
+    public static int quotient(int numerator, int denominator) {
+        // If the denominator is 0, throw an ArithmeticException
+        return numerator / denominator;
+    }
+
+    // Main method to test the quotient method
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // try block to catch exceptions
         try {
-            throw new InvalidInputException("Your input was invalid.");
-        } catch (InvalidInputException e) {
-            System.out.println(e.getMessage());
+            System.out.print("Please enter an integer numerator: ");
+            int numerator = scanner.nextInt();
+            // If the numerator is less than or equal to 0, throw an InvalidInputException
+            if(numerator <= 0) {
+                throw new InvalidInputException("You must enter positive numbers");
+            }
+
+            System.out.print("Please enter an integer denominator: ");
+            int denominator = scanner.nextInt();
+            // If the denominator is less than or equal to 0, throw an InvalidInputException
+            if(denominator <= 0) {
+                throw new InvalidInputException("You must enter positive numbers");
+            }
+            int result = quotient(numerator, denominator);
+            System.out.printf("%nResult: %d / %d = %d%n", numerator, denominator, result);
+        } catch (ArithmeticException | InvalidInputException e) { // using multi-catch here to catch multiple exceptions
+            // Print the error message if an exception is caught
+            System.out.println("Error: " + e.getMessage());
         }
     }
 }
--- src/Lab06/InvalidInputException.java
+++ src/Lab06/InvalidInputException.java
@@ -8,7 +8,14 @@
 
 package Lab06;
 
+// Custom exception class
 public class InvalidInputException extends Exception{
+    // Default constructor with a default message
+    public InvalidInputException() {
+        super("Your input was invalid.");
+    }
+
+    // Constructor with a custom message
     public InvalidInputException(String message) {
         super(message);
     }
