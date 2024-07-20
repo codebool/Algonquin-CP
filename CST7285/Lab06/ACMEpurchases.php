0255cf8280ed5747d99a52f8f67dd6943656d60f
Bo Qu <qu000030@algonquinlive.com>
2024-07-20 00:06:08 -0400

    update message

--- CST7285/Lab06/ACMEpurchases.php
+++ CST7285/Lab06/ACMEpurchases.php
@@ -14,7 +14,7 @@ unset($_SESSION['allTotal']); // Unset the allTotal session
     <link rel="stylesheet" href="./ACMEpurchase.css">
 </head>
 <body>
-<h2 id="purchaseMessage">Thanks for the purchase mate!</h2>
+<h2 id="purchaseMessage">thank you for your purchase</h2>
 <form action="index.php" method="GET">
     <button type="submit" id="back">Return to Order Page</button>
 </form>
