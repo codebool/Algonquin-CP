02b6f0f9eae05637042aed53d01b3d075182e3ee
CodeBool <52304840+codebool@users.noreply.github.com>
2024-09-13 15:59:33 -0400

    add TextFormatter.java saving the Employee object as key-value pairs.

---
+++ src/main/java/com/algonquin/cst8288/assignment1/persistence/TextFormatter.java
@@ -0,0 +1,35 @@
+/**
+ * Student Name: Bo Qu
+ * Lab Professor: Travis Lothar Czech
+ * Due Date: 2024-09-13
+ * Modified: 2024-09-13
+ * Description: Lab assignment
+ */
+
+package com.algonquin.cst8288.assignment1.persistence;
+
+import com.algonquin.cst8288.assignment1.emoloyee.Employee;
+
+import java.io.IOException;
+import java.util.StringJoiner;
+
+public class TextFormatter implements Formatter {
+    @Override
+    public String format(Employee employee) throws IOException {
+        if (employee == null) {
+            throw new IOException("Employee object is null");
+        }
+
+        StringJoiner joiner = new StringJoiner(", ");
+        joiner.add("name=" + employee.getName());
+        joiner.add("email=" + employee.getEmail());
+        joiner.add("address=" + employee.getAddress());
+        joiner.add("salary=" + employee.getSalary());
+        joiner.add("numberOfServiceYear=" + employee.getNumberOfServiceYear());
+        joiner.add("bonus=" + employee.getBonus());
+        joiner.add("totalCompensation=" + employee.getTotalCompensation());
+        joiner.add("renewalDate=" + employee.getRenewalDate());
+
+        return joiner.toString();
+    }
+}
