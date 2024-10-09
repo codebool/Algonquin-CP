d97a6dade941b4b164321582aa27442e1f96b8c6
CodeBool <52304840+codebool@users.noreply.github.com>
2024-10-09 16:26:48 -0400

    Init lab 02

---
+++ app/src/main/java/org/cst8288Lab2/App.java
@@ -0,0 +1,46 @@
+package org.cst8288Lab2;
+
+import java.io.*;
+import java.util.Properties;
+
+/**
+ * App
+ */
+public class App {
+    /**
+     * Parses the file: bulk-import.csv
+     * Validates each item in each row and updates the database accordingly.
+     *
+     * @param args -
+     */
+    public static void main(String[] args) {
+        String currentDir = System.getProperty("user.dir");
+        System.out.println("**** Current dir using System:" + currentDir);
+        //Ensure that you use the Properties class to load values from the database.properties file
+        Properties dbConnection = new Properties();
+
+        //Preserve this input path
+        try (InputStream in = new FileInputStream("D:\\Algonquin College\\2024_Fall\\CST7288_300 OOP\\Labs\\lab02\\cst8288-lab2\\cst8288-lab2-main\\app\\data\\database.properties")) {
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
+                String out;
+                while ((out = br.readLine()) != null) {
+                    System.out.println(out.toString());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //Preserve this input path
+        try (InputStream in = new FileInputStream("D:\\Algonquin College\\2024_Fall\\CST7288_300 OOP\\Labs\\lab02\\cst8288-lab2\\cst8288-lab2-main\\app\\data\\bulk-import.csv")) {
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
+                String out;
+                while ((out = br.readLine()) != null) {
+                    System.out.println(out.toString());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
