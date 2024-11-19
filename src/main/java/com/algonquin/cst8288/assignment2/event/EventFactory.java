aa40a3ce8f6941ff49923bf352803a9b77d64023
CodeBool <52304840+codebool@users.noreply.github.com>
2024-11-19 18:23:32 -0500

    Added logger

--- src/main/java/com/algonquin/cst8288/assignment2/event/EventFactory.java
+++ src/main/java/com/algonquin/cst8288/assignment2/event/EventFactory.java
@@ -8,16 +8,25 @@
 
 package com.algonquin.cst8288.assignment2.event;
 
+import com.algonquin.cst8288.assignment2.logger.LMSLogger;
+import com.algonquin.cst8288.assignment2.logger.LogLevel;
+
 public class EventFactory {
+    private static LMSLogger logger = LMSLogger.getInstance();
+
     public static EventCreator getEventCreator(EventType eventType) {
         switch (eventType) {
             case KIDS_STORY:
+                logger.log(LogLevel.INFO, "Creating KidsStorytimeEventCreator.");
                 return new KidsStorytimeEventCreator();
             case MOVIE_NIGHT:
+                logger.log(LogLevel.INFO, "Creating MovieNightEventCreator.");
                 return new MovieNightEventCreator();
             case WORKSHOP:
+                logger.log(LogLevel.INFO, "Creating WorkshopEventCreator.");
                 return new WorkshopEventCreator();
             default:
+                logger.log(LogLevel.ERROR, "Unknown event type: " + eventType);
                 throw new IllegalArgumentException("Unknown event type: " + eventType);
         }
     }
--- src/main/java/com/algonquin/cst8288/assignment2/logger/LMSLogger.java
+++ src/main/java/com/algonquin/cst8288/assignment2/logger/LMSLogger.java
@@ -6,9 +6,42 @@
  * Description: Lab assignment 2
  */
 
+// src/main/java/com/algonquin/cst8288/assignment2/logger/LMSLogger.java
 package com.algonquin.cst8288.assignment2.logger;
 
-public class LMSLogger {
-	
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
+public class LMSLogger {
+    private static LMSLogger instance;
+    private PrintWriter writer;
+
+    private LMSLogger() {
+        try {
+            FileWriter fileWriter = new FileWriter("application.log", true);
+            writer = new PrintWriter(fileWriter, true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static synchronized LMSLogger getInstance() {
+        if (instance == null) {
+            instance = new LMSLogger();
+        }
+        return instance;
+    }
+
+    public void log(LogLevel level, String message) {
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        writer.println(timestamp + " [" + level + "] " + message);
+    }
+
+    public void log(LogLevel level, String message, Throwable throwable) {
+        log(level, message);
+        throwable.printStackTrace(writer);
+    }
 }
